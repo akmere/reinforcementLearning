38551b8114dc7c201ee74d14f540a6ccb02d8789
Rafał Kamiński <rafal.kaminski@dxc.com>
2023-01-17 15:59:21 +0100

    little changes

--- ride.py
+++ ride.py
@@ -17,6 +17,7 @@ states = []
 starting_states = []
 terminal_states = []
 gamma = 1
+# np.random.seed(int(datetime.now().timestamp()))
 
 # velocities = list([[a, b] for a,b in itertools.product(range(-5,6), range(-5,6))])
 
@@ -42,9 +43,10 @@ actions = [[0, 0], [1, 0], [0, 1], [1, 1],
            [-1, 0], [0, -1], [-1, -1], [-1, 1], [1, -1]]
 actions = np.array(actions)
 tries = np.zeros((len(states),len(actions)))
-probs = np.zeros((len(states),len(actions)))
-probs.fill(1/len(actions))
+probs = np.full((len(states),len(actions)), 1/len(actions))
+pi = np.full((len(states),len(actions)), 1/len(actions))
 q = np.zeros((len(states),len(actions)))
+c = np.zeros((len(states),len(actions)))
 
 def get_state_index(state: State):
     start_time = datetime.now()
@@ -89,9 +91,11 @@ def do_step(state : State, action, velocity):
     # print(f"do_step: {end_time-start_time}")
     return new_state, velocity
 
-def do_episode(draw = False):
-    current_state = copy.deepcopy(np.random.choice(starting_states))
-    random_action = copy.deepcopy(actions[np.random.choice(len(actions))])
+def do_episode(b_policy, pi_policy, current_state = False, random_action = False, draw = False, improve_policy=True, use_proper_q = True):
+    if(current_state == False): current_state = np.random.choice(starting_states)
+    if(random_action == False): random_action = copy.deepcopy(actions[np.random.choice(len(actions))])
+    print(vars(current_state))
+    are_b_and_pi_policies_same = np.array_equal(b_policy, pi_policy)
     sa = [[current_state, random_action]]
     count = 0
     velocity = [0,0]
@@ -100,7 +104,7 @@ def do_episode(draw = False):
         count+=1
         # print(vars(current_state))
         state_index = get_state_index(current_state)
-        random_action = copy.deepcopy(actions[np.random.choice(len(actions), p=probs[state_index])])
+        random_action = copy.deepcopy(actions[np.random.choice(len(actions), p=b_policy[state_index])])
         # random_action = actions[probs[get_state_index(current_state)].argmax(0)]
         # print(random_action)
         current_state, velocity = do_step(current_state, random_action, velocity)
@@ -110,17 +114,25 @@ def do_episode(draw = False):
     sa.reverse()
 
     g = 0
+    w = 1
     for state_action in sa:
         if (draw): draw_map(state_action[0])
         state_index = get_state_index(state_action[0])
         action_index = np.where(actions == state_action[1])[0][0]
         g = rewards[state_index] + gamma * g
+        c[state_index,action_index] += w
         tries[state_index,action_index] += 1
-        q[state_index, action_index] = (q[state_index,action_index] * (tries[state_index,action_index] - 1) + g)/tries[state_index,action_index]
+        if(use_proper_q): q[state_index, action_index] += ((0 if c[state_index, action_index] == 0 else w/c[state_index, action_index])) * (g - q[state_index, action_index])
+        else: q[state_index, action_index] = (q[state_index,action_index] * (tries[state_index,action_index] - 1) + g)/tries[state_index,action_index]
         # probs[state_index] = q[state_index].argmax(0)
         maximizing_index = q[state_index].argmax(0)
-        probs[state_index].fill(0.1/(len(probs[state_index])-1))
-        probs[state_index,maximizing_index] = 0.90
+        if(improve_policy):
+            pi_policy[state_index].fill(0)
+            pi_policy[state_index,maximizing_index] = 1
+        if(not are_b_and_pi_policies_same): 
+            b_policy[state_index].fill(0.1/(len(b_policy[state_index])-1))
+            b_policy[state_index,maximizing_index] = 0.9
+        w = w/(b_policy[state_index, action_index]) if b_policy[state_index, action_index] != 0 else 0
         # print(probs[state_index])
         # print(q[state_index].argmax(0))
         # print(probs[state_index])
@@ -128,19 +140,19 @@ def do_episode(draw = False):
         # print(q[state_index, action_index])
     return count
 
+# probs.fill(1/len(actions))
+# pi.fill(1/len(actions))
+q = np.zeros((len(states),len(actions)))
+c = np.zeros((len(states),len(actions)))
 counts = []
 
-for i in range(5000):
+for i in range(1000):
     start_time = datetime.now()
-    count = do_episode()
+    count = do_episode(probs, pi)
     counts.append(count)
     print(f"{i}: {count}, {datetime.now() - start_time}")
 
 means = [np.mean(counts[:index+1]) for index, value in enumerate(counts)]
-
-do_episode(True)
-
-plt.plot(means)
+plt.plot(means, c='r')
 plt.xscale("log")
 plt.show()
-# print(probs)
